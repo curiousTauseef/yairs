c71e17046368e3d743d001e433c4fa7afcb66f75
hunterhector <hunterhector@gmail.com>
2013-03-16 23:37:06 -0400

    So this time the file provides Document frequency, although we still know it before, but this time we use the provided frequency instead

--- core/src/main/scala/yairs/model/InvertedList.scala
+++ core/src/main/scala/yairs/model/InvertedList.scala
@@ -11,7 +11,7 @@ import collection.mutable.ListBuffer
  * Date: 2/20/13
  * Time: 6:37 PM
  */
-case class InvertedList(term:String,stem:String,collectionFrequency:Int,totalTermCount:Int,postings:List[Posting]) extends Logging {
+case class InvertedList(term:String,stem:String,collectionFrequency:Int,totalTermCount:Int, documentFrequency: Int,postings:List[Posting]) extends Logging {
   def dump() {
     log.info(String.format("Dumping inverted list for [%s], with collection frequency [%s], total term count[%s]", term, collectionFrequency.toString, totalTermCount.toString))
     var termCount = 0
@@ -42,7 +42,6 @@ object InvertedList extends Logging{
 
   def apply(invertedFile:File, ranked:Boolean = true):InvertedList ={
     if (invertedFile.exists())  {
-
       val lines = Source.fromFile(invertedFile).getLines().toList
       val (term, stem, collectionFrequency, totalTermCount) = {
         val parts = lines(0).trim.split(" ")
@@ -61,12 +60,42 @@ object InvertedList extends Logging{
           tempPostings +=(new Posting(docId,tf,length,positions,1.0))
       })
 
-      new InvertedList(term, stem, collectionFrequency, totalTermCount,tempPostings.toList)
+      val postings = tempPostings.toList
+
+      new InvertedList(term, stem, collectionFrequency, totalTermCount,postings.length,postings)
     }else{
       log.error("This inverted list is not found: "+invertedFile.getCanonicalPath)
-      new InvertedList("","",0,0,List[Posting] ())
+      new InvertedList("","",0,0,0,List[Posting] ())
     }
   }
 
+  def apply(invertedFile:File,hasDocumentFreq:Boolean,ranked:Boolean = true):InvertedList = {
+    if (invertedFile.exists())  {
+      val lines = Source.fromFile(invertedFile).getLines().toList
+      val (term, stem, collectionFrequency, totalTermCount,documentFreq) = {
+        val parts = lines(0).trim.split(" ")
+        (parts(0), parts(1), parts(2).toInt, parts(3).toInt, parts(4).toInt)
+      }
+
+      var tempPostings = ListBuffer.empty[Posting]
+
+      lines.slice(1, lines.length).foreach(line => {
+        val parts = line.trim.split(" ")
+        val Array(docId, tf, length) = parts.slice(0, 3).map(str => str.toInt)
+        val positions = parts.slice(3, parts.length).map(str => str.toInt).toList
+        if (ranked)
+          tempPostings += (new Posting(docId,tf,length,positions,tf))
+        else
+          tempPostings +=(new Posting(docId,tf,length,positions,1.0))
+      })
+
+      val postings = tempPostings.toList
+
+      new InvertedList(term, stem, collectionFrequency, totalTermCount,documentFreq,postings)
+    }else{
+      log.error("This inverted list is not found: "+invertedFile.getCanonicalPath)
+      new InvertedList("","",0,0,0,List[Posting] ())
+    }
+  }
 
 }
